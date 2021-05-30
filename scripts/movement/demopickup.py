7350b6219f0d5b1c0f87459dfb1b352faa9a3bbb
ZhouXing19 <zhouxing@uchicago.edu>
2021-05-30 19:39:18 -0400

    [Zhou][passed] new launch, new dae, new gesture, picked up container

--- scripts/movement/demopickup.py
+++ scripts/movement/demopickup.py
@@ -12,10 +12,28 @@ from nav_msgs.msg import Odometry
 
 import moveit_commander
 
+# Define colors for the dumbbells and block number
+COLOR_BOUNDS = {'red': {'lb': np.array([0, 50, 0]),
+                            'ub': np.array([50, 255, 255])},
+                    'green': {'lb': np.array([50, 50, 0]),
+                            'ub': np.array([100, 255, 255])},
+                    'blue': {'lb': np.array([100, 50, 0]), 
+                            'ub': np.array([160, 255, 255])},
+                    'black': {'lb': np.array([0, 0, 0]), 
+                            'ub': np.array([180, 255, 50])}}
+
 MOVING_TO_BASKET = "moving to basket"
 MOVED_TO_BASKET = "moved to basket"
 HOLDING_BASKET = "holding basket"
 PULLING_BACK = "pulling basket"
+
+# Define robot statuses to keep track of its actions
+MEASURE_ANGLE = "measure_angle"
+GO_TO_DB = "go_to_dumbbell"
+REACHED_DB = "reached_db"
+PICKED_UP_DB = "picked_up_dumbbell"
+MOVING_TO_BLOCK = "moving_to_block"
+REACHED_BLOCK = "reached_block"
 
 class demoPickUp(object):
 
@@ -23,12 +41,19 @@ class demoPickUp(object):
 
         self.initialized = False
         rospy.init_node('demo_pickup')
-        
-        self.rb0_scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
-        self.rb0_cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
 
-        self.__goal_dist_in_front_basket = 0.24
-        self.__prop = 0.10
+
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
+        self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+
+        # Minimum distance in front of dumbbell and block
+        self.__goal_dist_in_front__db = 0.23
+
+        self.__goal_dist_in_front_basket = 0.22
+        self.__prop = 0.15
+
+        rospy.sleep(3)
 
         # The interface to the group of joints making up the turtlebot3
         #   openmanipulator arm
@@ -41,39 +66,78 @@ class demoPickUp(object):
         # Initialize starting robot arm and gripper positions
         self.initialize_move_group()
 
-        self.rb0_scan = []
+        # Set up ROS / cv bridge
+        self.bridge = cv_bridge.CvBridge()
+
+
+        # Initialize array to hold and process images
+        self.image = []
+
+        self.__scan_data = []
         self.rb1_scan = []
 
         self.scan_max = None
 
-        self.robot_status = MOVING_TO_BASKET
 
         if self.initialized == False:
             self.initialize_move_group()
+        
+        self.robot_status = MOVING_TO_BASKET
 
         # Now everything is initialized
+
+        print("initalized!!!13333")
         self.initialized = True
 
     
     def initialize_move_group(self):
-        arm_joint_goal = [0.0, 0.65, 0.15, -0.8]
+        """ Initialize the robot arm & gripper position so it can grab onto
+        the dumbbell """
+
+        # Set arm and gripper joint goals and move them
+        arm_joint_goal = [0.0, 0.70, 0.25, -1.0]
         gripper_joint_goal = [0.015, 0.015]
         self.move_group_arm.go(arm_joint_goal, wait=True)
         self.move_group_gripper.go(gripper_joint_goal, wait=True)
         self.move_group_arm.stop()
         self.move_group_gripper.stop()
         rospy.sleep(0.5)
+    
+    def lift_dumbbell(self):
+        """ Lift the dumbbell when robot reached the dumbbells """
+
+        # Do nothing if the robot hasn't reached the dumbbells
+        if self.robot_status != REACHED_DB:
+            return 
+
+        # Set arm and gripper joint goals and move them    
+        arm_joint_goal = [0.0, 0.05, -0.45, -0.1]
+        gripper_joint_goal = [0.004, 0.004]
+        self.move_group_arm.go(arm_joint_goal, wait=True)
+        self.move_group_gripper.go(gripper_joint_goal, wait=True)
+        self.move_group_arm.stop()
+        self.move_group_gripper.stop()
+
+        # Step back so the robot won't hit the dumbbell while rotating
+        print("----- stepping back!----")
+        self.pub_vel(0, -0.5)
+        rospy.sleep(0.8)
+        self.pub_vel(0, 0)
+
+        # After the robot grapped the dumbbells, it's time to identify the blocks
+        self.robot_status = HOLDING_BASKET
+
 
     def grab_basket(self):
         arm_joint_goal = [0.0, 0.05, -0.45, 0.4]
-        gripper_joint_goal = [-0.01, -0.01]
+        gripper_joint_goal = [0.004, 0.004]
         self.move_group_arm.go(arm_joint_goal, wait=True)
-        self.move_group_arm.stop()
         self.move_group_gripper.go(gripper_joint_goal, wait=True)
+        self.move_group_arm.stop()
         self.move_group_gripper.stop()
+        rospy.sleep(2)
         self.robot_status = HOLDING_BASKET
-        rospy.sleep(1.5)
-
+        
     def set_vel(self, diff_ang=0.0, diff_dist=float('inf')):
         """ Set the velocities of the robot """
 
@@ -86,7 +150,7 @@ class demoPickUp(object):
             lin_v = 0.0
         
         # Stop if the robot is in front of the dumbbell/block
-        elif diff_dist < self.__goal_dist_in_front_basket:
+        elif diff_dist < self.__goal_dist_in_front__db:
             print("=====Arrived!=====")
             ang_v, lin_v = 0.0, 0.0
         
@@ -99,21 +163,21 @@ class demoPickUp(object):
         return ang_v, lin_v
         
 
-    def pub_vel(self, pub_channel, ang_v=0.0, lin_v=0.0):
+    def pub_vel(self, ang_v=0.0, lin_v=0.0):
         """ Publish a twist to the cmd_vel channel """
 
         # Set linear and angular velocities and publish
         new_twist = Twist()
         new_twist.linear.x = lin_v
         new_twist.angular.z = ang_v
-        pub_channel.publish(new_twist)
+        self.cmd_vel_pub.publish(new_twist)
 
-    
+
     def stop_robot(self):
-        self.pub_vel(self.rb0_cmd_vel_pub, 0.0, 0.0)
+        self.pub_vel(0.0, 0.0)
     
     def step_back(self):
-        self.pub_vel(self.rb0_cmd_vel_pub, 0.0, -0.05)
+        self.pub_vel(0.0, -0.05)
         rospy.sleep(1)
 
 
@@ -122,46 +186,143 @@ class demoPickUp(object):
         if not self.initialized:
             return
         
-        self.rb0_scan = data.ranges
+        self.__scan_data = data.ranges
 
         if not self.scan_max:
             self.scan_max = data.range_max
+
+    def image_callback(self, data):
+        """ Process the image from the robot's RGB camera """
+
+        # Do nothing if initialization is not done
+        if not self.initialized:
+            return
+
+        # Take the ROS message with the image and turn it into a format cv2 can use
+        self.image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+
     
 
     
-    def move_to_basket(self):
+    def move_to_basket(self, color:str):
         # Now we are sure that the basket is in front of the robot
 
-        if len(self.rb0_scan) == 0:
+        if len(self.image) == 0:
+            print("-- Have not got the image --")
+            return
+
+        if len(self.__scan_data) == 0:
             print(" ---- no scan yet ---- ")
             return
 
-        min_dist = min(self.rb0_scan[-10:] + self.rb0_scan[:10])
+        min_dist = min(self.__scan_data[-10:] + self.__scan_data[:10])
         print(f"---- min_dist ----: {min_dist}")
         
         if min_dist <= self.__goal_dist_in_front_basket:
-            self.pub_vel(self.rb0_cmd_vel_pub, 0, 0)
+            self.pub_vel(0, 0)
             rospy.sleep(1)
 
             self.robot_status = MOVED_TO_BASKET
         else:
 
             ang_v, lin_v = self.set_vel(0, min_dist)
-            self.pub_vel(self.rb0_cmd_vel_pub, ang_v, lin_v)
+            self.pub_vel(ang_v, lin_v)
+    
+    def move_to_dumbbell(self, color: str):
+        """ Move to a dumbbell based on color """
+
+        # Do nothing if there are no images
+        if len(self.image) == 0:
+            print("-- Have not got the image --")
+            return
+
+        # Do nothing if there are no scan data
+        if len(self.__scan_data) == 0:
+            print("-- Have not got the scan --")
+            return
+
+        # Turn image into HSV style
+        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        
+        # Get lower and upper bounds of the specified color
+        lb, ub = COLOR_BOUNDS[color]['lb'], COLOR_BOUNDS[color]['ub']
+        
+        # Mask and get moment of the color of the dumbbell
+        mask = cv2.inRange(hsv, lb, ub)
+        M = cv2.moments(mask)
+
+        # Get the shape of the image to compute its center
+        h, w, d = self.image.shape
+
+        # If there are any pixels found for the desired color
+        if M['m00'] > 0:
+
+            # Determine the center of the colored pixels in the image
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+
+            # Dumbbell's distance to the center of the camera
+            err = w/2 - cx
+
+            print(f"abs(err) / w : {abs(err) / w}")
+
+            # If the color center is at the front
+            if abs(err) / w < 0.05:
+                
+                min_dist = min(self.__scan_data[-10:] + self.__scan_data[:10])
+
+                print(f"min_dist: {min_dist}")
+
+                # If the robot is close to the dumbbell
+                if min_dist <= self.__goal_dist_in_front__db:
+
+                    # Stop the robot
+                    self.pub_vel(0,0)
+
+                    # Sleep 1s to make sure the robot has stopped
+                    rospy.sleep(1)
+
+                    # Change status to reached dumbbell
+                    self.robot_status = REACHED_DB
+                    print(f"---reached dumbbell of color {color}----")
+
+                    # Lift the dumbbell
+                    self.lift_dumbbell()
+
+                else:
+
+                    # Rush STRAIGHT toward the dumbbell
+                    ang_v, lin_v = self.set_vel(0, min_dist)
+                    self.pub_vel(ang_v, lin_v)
+
+            # If the color center is not right at the front yet
+            else:
+                
+                # Define k_p for proportional control            
+                k_p = 1.0 / 1000.0
+
+                # Slowly turn the head, so that the color center 
+                #   would be at the center of the camera
+                self.pub_vel(k_p * err, 0)
+                print(f"---turning to dumbbell of color {color}----")
+
+        # If we cannot see any pixel of the desired color
+        else:
+
+            # Simply turn the head clockwise, without any linear speed
+            ang_v, lin_v = self.set_vel()
+            self.pub_vel(ang_v, lin_v)
+
+
     
     def run(self):
-        r = rospy.Rate(10)
+        r = rospy.Rate(5)
 
         while not rospy.is_shutdown():
 
             if self.robot_status == MOVING_TO_BASKET:
-                self.move_to_basket()
-            elif self.robot_status == MOVED_TO_BASKET:
-                self.grab_basket()
-            elif self.robot_status == HOLDING_BASKET:
-                self.step_back()
-            elif self.robot_status == PULLING_BACK:
-                self.stop_robot()
+                self.move_to_dumbbell(color="red")
+
             
             r.sleep()
 
